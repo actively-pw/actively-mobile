5e02e686e84e15d2dbe32f3d6c2da399f650ead1
Szymon022 <szymon.kurpas00@gmail.com>
2023-08-03 20:10:06 +0200

    Add LocationProvider dependency

--- app/src/main/java/com/actively/KoinSetup.kt
+++ app/src/main/java/com/actively/KoinSetup.kt
@@ -1,12 +1,22 @@
 package com.actively
 
 import android.content.Context
+import com.actively.location.LocationProvider
+import com.actively.location.LocationProviderImpl
+import com.mapbox.common.location.compat.LocationEngineProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
 object KoinSetup {
 
     fun initKoin(context: Context) = startKoin {
         androidContext(context)
+        modules(commonModule)
+    }
+
+    val commonModule = module {
+        single { LocationEngineProvider.getBestLocationEngine(androidContext()) }
+        single<LocationProvider> { LocationProviderImpl(get()) }
     }
 }
