9597a726bd2275efd123bc07c32f18f3beb39a49
Szymon022 <szymon.kurpas00@gmail.com>
2023-08-08 18:09:19 +0200

    Improve ActivityDataSourceTest

--- app/src/test/java/com/actively/datasource/ActivityDataSourceTest.kt
+++ app/src/test/java/com/actively/datasource/ActivityDataSourceTest.kt
@@ -3,97 +3,82 @@ package com.actively.datasource
 import app.cash.sqldelight.driver.jdbc.sqlite.JdbcSqliteDriver
 import com.actively.ActivityDatabase
 import com.actively.activity.Activity
-import com.actively.activity.Location
-import com.actively.activity.Route
+import com.actively.stubs.stubActivity
+import com.actively.stubs.stubActivityStats
+import com.actively.stubs.stubRoute
+import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.test.testCoroutineScheduler
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
-import kotlinx.datetime.Instant
-import kotlin.time.Duration.Companion.hours
 
 @OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
 class ActivityDataSourceTest : FunSpec({
+
+    coroutineTestScope = true
+    isolationMode = IsolationMode.InstancePerTest
 
     val driver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
     val database = ActivityDatabase(driver).apply {
         ActivityDatabase.Schema.create(driver)
     }
 
-    test("Should insert and retrieve activity properly") {
+    context("ActivityDataSourceTest") {
         val activityDataSource = ActivityDataSourceImpl(database, testCoroutineScheduler)
-        val expectedActivity = Activity(
-            id = Activity.Id(1L),
-            sport = "Cycling",
-            start = Instant.fromEpochMilliseconds(0),
-            totalTime = 1.hours,
-            totalDistance = 23.5,
-            averageSpeed = 20.3
-        )
-        activityDataSource.insertActivity(expectedActivity)
-        activityDataSource.getActivity(id = Activity.Id(1L)).shouldBe(expectedActivity)
-    }
 
-    test("Should return list of all activities") {
-        val activityDataSource = ActivityDataSourceImpl(database, testCoroutineScheduler)
-        val expectedActivities = listOf(
-            Activity(
-                id = Activity.Id(1L),
-                sport = "Cycling",
-                start = Instant.fromEpochMilliseconds(0),
-                totalTime = 1.hours,
-                totalDistance = 23.5,
-                averageSpeed = 20.3
-            ),
-            Activity(
-                id = Activity.Id(2L),
-                sport = "Running",
-                start = Instant.fromEpochMilliseconds(0),
-                totalTime = 2.hours,
-                totalDistance = 20.0,
-                averageSpeed = 10.0
+        test("Should retrieve list of activities") {
+            val activities = listOf(
+                stubActivity(id = "1"),
+                stubActivity(id = "2"),
+                stubActivity(id = "3"),
+                stubActivity(id = "4"),
+                stubActivity(id = "5"),
+                stubActivity(id = "6"),
+                stubActivity(id = "7"),
+                stubActivity(id = "8"),
+                stubActivity(id = "9"),
             )
-        )
-        expectedActivities.forEach {
-            activityDataSource.insertActivity(it)
+            activities.forEach {
+                activityDataSource.insertActivity(it)
+            }
+            activityDataSource.getActivities().first() shouldBe activities
         }
-        activityDataSource.getActivities().first().shouldBe(expectedActivities)
-        testCoroutineScheduler.advanceUntilIdle()
-    }
 
-    test("Should insert and retrieve Route properly") {
-        val activityDataSource = ActivityDataSourceImpl(database, testCoroutineScheduler)
-        val expectedRoute = Route(
-            id = Route.Id(1L),
-            activityId = Activity.Id(1L),
-            locations = listOf(
-                Location(1.0, 1.0, Instant.fromEpochMilliseconds(0)),
-                Location(2.0, 2.0, Instant.fromEpochMilliseconds(0)),
-                Location(3.0, 3.0, Instant.fromEpochMilliseconds(0)),
-                Location(4.0, 4.0, Instant.fromEpochMilliseconds(0)),
-            )
-        )
-        activityDataSource.insertRoute(expectedRoute)
-        activityDataSource.getRoute(id = Activity.Id(1L)).shouldBe(expectedRoute)
-    }
-
-    test("Should insert and retrieve Location points properly") {
-        val activityDataSource = ActivityDataSourceImpl(database, testCoroutineScheduler)
-        val expectedLocations = listOf(
-            Location(1.0, 1.0, Instant.fromEpochMilliseconds(0)),
-            Location(2.0, 2.0, Instant.fromEpochMilliseconds(0)),
-            Location(3.0, 3.0, Instant.fromEpochMilliseconds(0)),
-            Location(4.0, 4.0, Instant.fromEpochMilliseconds(0)),
-        )
-        expectedLocations.forEach {
-            activityDataSource.insertLocation(location = it, routeId = Route.Id(2L))
+        test("Should retrieve empty list of activities if none were found") {
+            activityDataSource.getActivities().first().shouldBeEmpty()
         }
-        activityDataSource.getRouteLocations(id = Route.Id(2L)).shouldBe(expectedLocations)
-    }
 
-}) {
-    init {
-        coroutineTestScope = true
+        test("Should insert and retrieve activity properly") {
+            val activity = stubActivity(id = "1")
+            activityDataSource.insertActivity(activity)
+            activityDataSource.getActivity(id = activity.id) shouldBe activity
+        }
+
+        test("Should return null Activity if none were found") {
+            activityDataSource.getActivity(id = Activity.Id("1")).shouldBeNull()
+        }
+
+        test("Should insert and retrieve activity stats properly") {
+            val stats = stubActivityStats()
+            activityDataSource.insertStats(stats = stats, id = Activity.Id("1"))
+            activityDataSource.getStats(id = Activity.Id("1")) shouldBe stats
+        }
+
+        test("Should return null Activity.Stats if none were found") {
+            activityDataSource.getStats(id = Activity.Id("1")).shouldBeNull()
+        }
+
+        test("Should insert and retrieve route properly") {
+            val route = stubRoute(locationsNumber = 10)
+            activityDataSource.insertRoute(route = route, id = Activity.Id("1"))
+            activityDataSource.getRoute(id = Activity.Id("1")) shouldBe route
+        }
+
+        test("Should return empty route if none were found") {
+            activityDataSource.getRoute(id = Activity.Id("1")).shouldBeEmpty()
+        }
     }
-}
+})
