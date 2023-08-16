542c9312da0b6af047f53725155b8020f84bd62e
Szymon022 <szymon.kurpas00@gmail.com>
2023-08-16 07:59:53 +0200

    Update ActivityRepositoryTest

--- app/src/test/java/com/actively/repository/ActivityRepositoryTest.kt
+++ app/src/test/java/com/actively/repository/ActivityRepositoryTest.kt
@@ -5,20 +5,22 @@ import com.actively.datasource.ActivityDataSource
 import com.actively.stubs.stubActivity
 import com.actively.stubs.stubActivityStats
 import com.actively.stubs.stubLocation
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.core.test.testCoroutineScheduler
 import io.kotest.data.forAll
 import io.kotest.data.row
 import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 
-@OptIn(ExperimentalStdlibApi::class, ExperimentalCoroutinesApi::class)
 class ActivityRepositoryTest : FunSpec({
 
     coroutineTestScope = true
@@ -28,7 +30,7 @@ class ActivityRepositoryTest : FunSpec({
 
 
     context("getActivities()") {
-        val repository = ActivityRepositoryImpl(activityDataSource, testCoroutineScheduler)
+        val repository = ActivityRepositoryImpl(activityDataSource)
         val activities = listOf(stubActivity(id = "1"), stubActivity(id = "2"))
         every { activityDataSource.getActivities() } returns flowOf(activities)
 
@@ -43,9 +45,9 @@ class ActivityRepositoryTest : FunSpec({
     }
 
     context("getActivity(id: Activity.Id)") {
-        val repository = ActivityRepositoryImpl(activityDataSource, testCoroutineScheduler)
-        every { activityDataSource.getActivity(any()) } returns null
-        every { activityDataSource.getActivity(id = Activity.Id("1")) } returns stubActivity(id = "1")
+        val repository = ActivityRepositoryImpl(activityDataSource)
+        coEvery { activityDataSource.getActivity(any()) } returns null
+        coEvery { activityDataSource.getActivity(id = Activity.Id("1")) } returns stubActivity(id = "1")
 
         test("Should return activity with given id") {
             forAll(
@@ -62,22 +64,26 @@ class ActivityRepositoryTest : FunSpec({
                 row(Activity.Id("non-existing-id")),
             ) { id ->
                 repository.getActivity(id)
-                verify(exactly = 1) { activityDataSource.getActivity(id) }
+                coVerify(exactly = 1) { activityDataSource.getActivity(id) }
             }
         }
     }
 
     context("getStats(id: Activity.Id)") {
-        val repository = ActivityRepositoryImpl(activityDataSource, testCoroutineScheduler)
-        every { activityDataSource.getStats(any()) } returns null
-        every { activityDataSource.getStats(id = Activity.Id("1")) } returns stubActivityStats()
+        val repository = ActivityRepositoryImpl(activityDataSource)
+        every { activityDataSource.getStats(any()) } returns flow { throw IllegalArgumentException() }
+        every { activityDataSource.getStats(id = Activity.Id("1")) } returns flowOf(
+            stubActivityStats()
+        )
 
         test("Should return Stats for activity given by id") {
-            forAll(
-                row(Activity.Id("1"), stubActivityStats()),
-                row(Activity.Id("non-existing-id"), null)
-            ) { id, stats ->
-                repository.getStats(id) shouldBe stats
+            val expected = stubActivityStats()
+            repository.getStats(id = Activity.Id("1")).first() shouldBe expected
+        }
+
+        test("Should throw exception for non non existing activity") {
+            shouldThrow<IllegalArgumentException> {
+                repository.getStats(id = Activity.Id("non-existing-activity")).collect()
             }
         }
 
@@ -93,26 +99,26 @@ class ActivityRepositoryTest : FunSpec({
     }
 
     context("insert*()") {
-        val repository = ActivityRepositoryImpl(activityDataSource, testCoroutineScheduler)
+        val repository = ActivityRepositoryImpl(activityDataSource)
 
         test("Should call ActivityDataSource insertActivity with given activity") {
             val activity = stubActivity()
             repository.insertActivity(activity)
-            verify(exactly = 1) { activityDataSource.insertActivity(activity) }
+            coVerify(exactly = 1) { activityDataSource.insertActivity(activity) }
         }
 
         test("Should call ActivityDataSource insertStats with given stats and id") {
             val stats = stubActivityStats()
             val id = Activity.Id("1")
             repository.insertStats(stats, id)
-            verify(exactly = 1) { activityDataSource.insertStats(stats, id) }
+            coVerify(exactly = 1) { activityDataSource.insertStats(stats, id) }
         }
 
         test("Should call ActivityDataSource insertLocation with given location and id") {
             val location = stubLocation()
             val id = Activity.Id("1")
             repository.insertLocation(location, id)
-            verify(exactly = 1) { activityDataSource.insertLocation(location, id) }
+            coVerify(exactly = 1) { activityDataSource.insertLocation(location, id) }
         }
     }
 })
