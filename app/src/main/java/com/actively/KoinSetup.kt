682dba320f7a04aefabc2afb301628ad13691f93
Szymon022 <szymon.kurpas00@gmail.com>
2023-08-16 19:55:06 +0200

    Clean up KoinSetup modules

--- app/src/main/java/com/actively/KoinSetup.kt
+++ app/src/main/java/com/actively/KoinSetup.kt
@@ -5,8 +5,6 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
 import com.actively.location.LocationProvider
 import com.actively.location.LocationProviderImpl
-import com.actively.location.usecase.GetUserLocationUpdatesUseCase
-import com.actively.location.usecase.GetUserLocationUpdatesUseCaseImpl
 import com.mapbox.common.location.compat.LocationEngineProvider
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
@@ -20,7 +18,6 @@ object KoinSetup {
     }
 
     private val useCasesModule = module {
-        factory<GetUserLocationUpdatesUseCase> { GetUserLocationUpdatesUseCaseImpl(get()) }
     }
 
     private val commonModule = module {
