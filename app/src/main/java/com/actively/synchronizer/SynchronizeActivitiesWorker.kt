373e7fff1bcc17deee5bc3f76e56c22a43368863
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-18 21:13:05 +0200

    Add SynchronizeActivitiesWorker

---
+++ app/src/main/java/com/actively/synchronizer/SynchronizeActivitiesWorker.kt
@@ -0,0 +1,23 @@
+package com.actively.synchronizer
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.actively.synchronizer.usecases.SynchronizeActivitiesUseCase
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class SynchronizeActivitiesWorker(
+    context: Context,
+    params: WorkerParameters
+) : CoroutineWorker(context, params), KoinComponent {
+
+    private val synchronizeActivitiesUseCase by inject<SynchronizeActivitiesUseCase>()
+
+    override suspend fun doWork(): Result = try {
+        synchronizeActivitiesUseCase()
+        Result.success()
+    } catch (e: Exception) {
+        Result.retry()
+    }
+}
