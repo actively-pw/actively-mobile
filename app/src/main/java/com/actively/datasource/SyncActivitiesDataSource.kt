1ddb9d73f413ce98d58d255e782011f657307cab
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-18 19:47:59 +0200

    Add SyncActivitiesDataSource

---
+++ app/src/main/java/com/actively/datasource/SyncActivitiesDataSource.kt
@@ -0,0 +1,24 @@
+package com.actively.datasource
+
+
+import com.actively.activity.Activity
+import io.ktor.client.HttpClient
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+interface SyncActivitiesDataSource {
+
+    suspend fun syncActivity(activity: Activity)
+}
+
+class SyncActivitiesDataSourceImpl(private val client: HttpClient) : SyncActivitiesDataSource {
+
+    override suspend fun syncActivity(activity: Activity) {
+        client.post("https://activelypw.azurewebsites.net/Activities") {
+            contentType(ContentType.Application.Json)
+            setBody(activity)
+        }
+    }
+}
