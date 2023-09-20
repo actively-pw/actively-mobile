a7717b543603e96680026cc13678af09d6861d9c
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-20 13:40:55 +0200

    Add textfield to title ride

--- app/src/main/java/com/actively/recorder/ui/SaveRecordingScreen.kt
+++ app/src/main/java/com/actively/recorder/ui/SaveRecordingScreen.kt
@@ -1,30 +1,56 @@
 package com.actively.recorder.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.actively.R
 import com.actively.ui.theme.ActivelyTheme
+import org.koin.androidx.compose.getViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SaveRecordingScreen() {
+fun SaveRecordingScreen(viewModel: SaveActivityViewModel = getViewModel()) {
     ActivelyTheme {
         Scaffold(
             topBar = {
                 TopBar(onBackClick = { }, onSaveClick = {})
             }
         ) {
-
+            val title by viewModel.title.collectAsState()
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    label = { Text(stringResource(id = R.string.title_label)) },
+                    value = title,
+                    onValueChange = viewModel::onTitleChange
+                )
+            }
         }
     }
 }
@@ -50,5 +76,5 @@ private fun TopBar(onBackClick: () -> Unit, onSaveClick: () -> Unit) {
 @Preview
 @Composable
 fun SaveRecordingScreenPreview() {
-    SaveRecordingScreen()
+    SaveRecordingScreen(SaveActivityViewModel())
 }
