ac0b77ff97ea6efcd5bb31f92cc99efcfc06d80d
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-20 14:01:20 +0200

    Add discard activity button

--- app/src/main/java/com/actively/recorder/ui/SaveActivityViewModel.kt
+++ app/src/main/java/com/actively/recorder/ui/SaveActivityViewModel.kt
@@ -1,16 +1,27 @@
 package com.actively.recorder.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class SaveActivityViewModel : ViewModel() {
 
     private val _title = MutableStateFlow("")
     val title = _title.asStateFlow()
 
+    private val _showDiscardDialog = MutableSharedFlow<Boolean>()
+    val showDiscardDialog = _showDiscardDialog.asSharedFlow()
+
     fun onTitleChange(title: String) {
         _title.update { title }
+    }
+
+    fun onDiscardClick() = viewModelScope.launch {
+        _showDiscardDialog.emit(true)
     }
 }
--- app/src/main/java/com/actively/recorder/ui/SaveRecordingScreen.kt
+++ app/src/main/java/com/actively/recorder/ui/SaveRecordingScreen.kt
@@ -1,14 +1,20 @@
 package com.actively.recorder.ui
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -50,6 +56,14 @@ fun SaveRecordingScreen(viewModel: SaveActivityViewModel = getViewModel()) {
                     value = title,
                     onValueChange = viewModel::onTitleChange
                 )
+                Spacer(Modifier.height(24.dp))
+                DiscardActivityButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(12.dp)
+                        .align(Alignment.End),
+                    onClick = viewModel::onDiscardClick
+                )
             }
         }
     }
@@ -71,6 +85,23 @@ private fun TopBar(onBackClick: () -> Unit, onSaveClick: () -> Unit) {
             }
         }
     )
+}
+
+@Composable
+fun DiscardActivityButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    OutlinedButton(
+        modifier = modifier,
+        colors = ButtonDefaults.outlinedButtonColors(
+            contentColor = MaterialTheme.colorScheme.error
+        ),
+        border = BorderStroke(
+            1.dp,
+            MaterialTheme.colorScheme.error
+        ),
+        onClick = onClick
+    ) {
+        Text(stringResource(id = R.string.discard_activity))
+    }
 }
 
 @Preview
