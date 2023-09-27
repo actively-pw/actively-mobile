93a19b4adf23feae5f99b7e07567771b9ce7ac0c
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-27 13:01:37 +0200

    Test mapping to RecordedActivity

---
+++ app/src/test/java/com/actively/http/dtos/RecordedActivityDtoTest.kt
@@ -0,0 +1,28 @@
+package com.actively.http.dtos
+
+import com.actively.activity.Activity
+import com.actively.activity.RecordedActivity
+import com.actively.distance.Distance.Companion.kilometers
+import com.actively.stubs.stubRecordedActivityDto
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlin.time.Duration.Companion.hours
+
+class RecordedActivityDtoTest : FunSpec({
+
+    test("Should properly map RecordedActivityDto to RecordedActivity") {
+        val dto = stubRecordedActivityDto()
+        val recordedActivity = RecordedActivity(
+            id = RecordedActivity.Id("1"),
+            title = "Morning activity",
+            sport = "",
+            stats = Activity.Stats(
+                totalTime = 5.hours,
+                averageSpeed = 20.0,
+                distance = 100.kilometers
+            ),
+            routeUrl = "route://activity.net/1"
+        )
+        dto.toRecordedActivity() shouldBe recordedActivity
+    }
+})
