1a37061a362153a1322099af3802895239bfedbc
Szymon022 <szymon.kurpas00@gmail.com>
2023-09-29 09:05:35 +0200

    Wrap HomeScreen with BaseScaffoldScreen

--- app/src/main/java/com/actively/home/ui/HomeScreen.kt
+++ app/src/main/java/com/actively/home/ui/HomeScreen.kt
@@ -3,51 +3,45 @@ package com.actively.home.ui
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.actively.BaseScaffoldScreen
 import com.actively.activity.RecordedActivity
 import com.actively.ui.theme.ActivelyTheme
 import org.koin.androidx.compose.getViewModel
 
-fun NavGraphBuilder.homeScreen() {
+fun NavGraphBuilder.homeScreen(navController: NavController) {
     composable("home_screen") {
         val viewModel: HomeViewModel = getViewModel()
         val activities = viewModel.activitiesPager.flow.collectAsLazyPagingItems()
-        HomeScreen(activities = activities)
+        ActivelyTheme {
+            BaseScaffoldScreen(navController = navController) {
+                HomeScreen(activities = activities)
+            }
+        }
+
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     activities: LazyPagingItems<RecordedActivity>,
 ) {
-    ActivelyTheme {
-        Scaffold { contentPadding ->
-            LazyColumn(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(contentPadding)
-            ) {
-                items(count = activities.itemCount) { index ->
-                    activities[index]?.let {
-                        RecordedActivityItem(recordedActivity = it)
-                        if (index != activities.itemCount - 1) {
-                            Spacer(Modifier.height(6.dp))
-                        }
-                    }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(count = activities.itemCount) { index ->
+            activities[index]?.let {
+                RecordedActivityItem(recordedActivity = it)
+                if (index != activities.itemCount - 1) {
+                    Spacer(Modifier.height(6.dp))
                 }
             }
         }
     }
 }
-
