b299d2fab4cbb62e39382faf13cc9d8c243ddfed
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-01 18:53:36 +0200

    Add progress indicators on initial home screen load and on append

--- app/src/main/java/com/actively/home/ui/HomeScreen.kt
+++ app/src/main/java/com/actively/home/ui/HomeScreen.kt
@@ -1,15 +1,21 @@
 package com.actively.home.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import com.actively.activity.RecordedActivity
@@ -35,11 +41,34 @@ fun HomeScreen(
     activities: LazyPagingItems<RecordedActivity>,
 ) {
     LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(count = activities.itemCount) { index ->
-            activities[index]?.let {
-                RecordedActivityItem(recordedActivity = it)
-                if (index != activities.itemCount - 1) {
-                    Spacer(Modifier.height(6.dp))
+        if (activities.loadState.refresh == LoadState.Loading) {
+            item {
+                Column(
+                    modifier = Modifier.fillParentMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        } else {
+            items(count = activities.itemCount) { index ->
+                activities[index]?.let {
+                    RecordedActivityItem(recordedActivity = it)
+                    if (index != activities.itemCount - 1) {
+                        Spacer(Modifier.height(6.dp))
+                    }
+                }
+            }
+            if (activities.loadState.append == LoadState.Loading) {
+                item {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(75.dp)
+                    ) {
+                        CircularProgressIndicator()
+                    }
                 }
             }
         }
