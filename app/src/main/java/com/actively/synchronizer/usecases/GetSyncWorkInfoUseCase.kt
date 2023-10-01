27d8556b1576ed88b539b87909c295f16c7d8c66
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-01 20:38:20 +0200

    Add mapping to new WorkState

--- app/src/main/java/com/actively/synchronizer/usecases/GetSyncWorkInfoUseCase.kt
+++ app/src/main/java/com/actively/synchronizer/usecases/GetSyncWorkInfoUseCase.kt
@@ -3,17 +3,28 @@ package com.actively.synchronizer.usecases
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.actively.synchronizer.SynchronizeActivitiesWorker
+import com.actively.synchronizer.WorkState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 interface GetSyncWorkInfoUseCase {
 
-    operator fun invoke(): Flow<WorkInfo?>
+    operator fun invoke(): Flow<WorkState?>
 }
 
 class GetSyncWorkInfoUseCaseImpl(private val workManager: WorkManager) : GetSyncWorkInfoUseCase {
 
     override fun invoke() = workManager
         .getWorkInfosForUniqueWorkFlow(SynchronizeActivitiesWorker.SYNC_WORK_NAME)
-        .map { it.firstOrNull() }
+        .map {
+            it.firstOrNull()?.let { workInfo ->
+                when {
+                    workInfo.stopReason == WorkInfo.STOP_REASON_CONSTRAINT_CONNECTIVITY -> WorkState.NoInternetConnection
+                    workInfo.state == WorkInfo.State.ENQUEUED -> WorkState.Enqueued
+                    workInfo.state == WorkInfo.State.RUNNING -> WorkState.Running
+                    else -> null
+                }
+            }
+        }
 }
+
