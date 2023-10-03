9cec931008f7707f79d146f27ea0d71149c37e55
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-03 11:31:32 +0200

    Add TopAppBar to recorderScreen with back button

--- app/src/main/java/com/actively/recorder/ui/RecorderScreen.kt
+++ app/src/main/java/com/actively/recorder/ui/RecorderScreen.kt
@@ -16,25 +16,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.actively.BuildConfig
+import com.actively.R
 import com.actively.map.RecorderMap
 import com.actively.permissions.requestPermissionsIfNotGranted
 import com.actively.recorder.RecorderState
@@ -63,11 +70,12 @@ fun NavGraphBuilder.recorderScreen(navController: NavController) {
             onStopRecordingClick = { navController.navigate("save_screen") },
             onShowPermissionRequestDialog = viewModel::showRequestPermissionDialog,
             onDismissPermissionDialog = viewModel::dismissRequestPermissionDialog,
+            onBackClick = { navController.popBackStack() }
         )
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
 private fun RecorderScreen(
     stats: StatisticsState,
@@ -80,9 +88,10 @@ private fun RecorderScreen(
     onStopRecordingClick: () -> Unit,
     onShowPermissionRequestDialog: () -> Unit,
     onDismissPermissionDialog: () -> Unit,
+    onBackClick: () -> Unit,
 ) {
     ActivelyTheme {
-        Scaffold {
+        Scaffold(topBar = { AppBar(onBackClick = onBackClick) }) {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
@@ -133,6 +142,19 @@ private fun RecorderScreen(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun AppBar(onBackClick: () -> Unit) {
+    TopAppBar(
+        title = { Text(stringResource(R.string.ride)) },
+        navigationIcon = {
+            IconButton(onClick = onBackClick) {
+                Icon(Icons.Default.ArrowBack, contentDescription = null)
+            }
+        }
+    )
 }
 
 @Composable
