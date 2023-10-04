3450528163269e06a1605f81fbb2a88adeafdf89
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-04 18:48:47 +0200

    Improve LoginViewModel validation logic

--- app/src/main/java/com/actively/auth/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/actively/auth/ui/login/LoginViewModel.kt
@@ -14,11 +14,35 @@ class LoginViewModel : ViewModel() {
     private val _password = MutableStateFlow(TextFieldState(value = ""))
     val password = _password.asStateFlow()
 
+    private val emailRegex = Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}\$")
+
     fun onEmailChange(value: String) {
-        _email.update { it.copy(value = value) }
+        _email.update {
+            it.copy(
+                value = value,
+                isError = if (it.isError) !isEmailValid(value) else it.isError
+            )
+        }
     }
 
     fun onPasswordChange(value: String) {
-        _password.update { it.copy(value = value) }
+        _password.update {
+            it.copy(
+                value = value,
+                isError = if (it.isError) !isPasswordValid(value) else it.isError
+            )
+        }
     }
+
+    fun validateFields(onSuccess: () -> Unit) {
+        _email.update { it.copy(isError = !isEmailValid(it.value)) }
+        _password.update { it.copy(isError = !isPasswordValid(it.value)) }
+        if (!_email.value.isError && !_password.value.isError) {
+            onSuccess()
+        }
+    }
+
+    private fun isEmailValid(email: String) = email.isNotEmpty() && emailRegex.matches(email)
+
+    private fun isPasswordValid(password: String) = password.length in 8..50
 }
