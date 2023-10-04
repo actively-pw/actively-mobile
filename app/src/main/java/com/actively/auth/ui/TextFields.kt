a629552298d73c2a422ad4dc4270f499140a0584
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-04 16:39:44 +0200

    Add basic PasswordTextField

--- app/src/main/java/com/actively/auth/ui/TextFields.kt
+++ app/src/main/java/com/actively/auth/ui/TextFields.kt
@@ -1,5 +1,7 @@
 package com.actively.auth.ui
 
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -9,6 +11,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import com.actively.R
 
 @Composable
@@ -24,5 +27,29 @@ fun EmailTextField(value: String, onValueChange: (String) -> Unit, modifier: Mod
             keyboardType = KeyboardType.Password,
             imeAction = ImeAction.Next
         )
+    )
+}
+
+@Composable
+fun PasswordTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    onDone: (KeyboardActionScope.() -> Unit)? = null
+) {
+    OutlinedTextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(R.string.password)) },
+        supportingText = { Text(stringResource(R.string.password_textfield_supporting_text)) },
+        visualTransformation = PasswordVisualTransformation(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            capitalization = KeyboardCapitalization.None,
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(onDone = onDone)
     )
 }
