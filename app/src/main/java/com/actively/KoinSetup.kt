d6d3a0289261e5b660fea662ebcbd73996325684
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-04 14:42:56 +0200

    Update Koin dependencies

--- app/src/main/java/com/actively/KoinSetup.kt
+++ app/src/main/java/com/actively/KoinSetup.kt
@@ -8,6 +8,8 @@ import com.actively.activity.usecase.CreateActivityUseCase
 import com.actively.activity.usecase.CreateActivityUseCaseImpl
 import com.actively.datasource.ActivityRecordingDataSource
 import com.actively.datasource.ActivityRecordingDataSourceImpl
+import com.actively.datasource.RecordedActivitiesDataSource
+import com.actively.datasource.RecordedActivitiesDataSourceImpl
 import com.actively.datasource.SyncActivitiesDataSource
 import com.actively.datasource.SyncActivitiesDataSourceImpl
 import com.actively.datasource.factory.RecordedActivitiesDataSourceFactory
@@ -132,6 +134,7 @@ object KoinSetup {
         single<ActivityDatabase> { ActivityDatabase(get()) }
         single<ActivityRecordingDataSource> { ActivityRecordingDataSourceImpl(get()) }
         single<SyncActivitiesDataSource> { SyncActivitiesDataSourceImpl(get()) }
+        single<RecordedActivitiesDataSource> { RecordedActivitiesDataSourceImpl(get()) }
         single<ActivityRecordingRepository> { ActivityRecordingRepositoryImpl(get(), get()) }
         single<TimeProvider> { TimeProvider(Clock.System::now) }
         single<UUIDProvider> { UUIDProviderImpl() }
