25c9a8e375b37d6a2ae79c882ef931314766789e
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-04 15:12:29 +0200

    Fix preview

--- app/src/main/java/com/actively/home/ui/EmptyActivitiesListItem.kt
+++ app/src/main/java/com/actively/home/ui/EmptyActivitiesListItem.kt
@@ -3,7 +3,6 @@ package com.actively.home.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
@@ -43,9 +42,6 @@ fun EmptyActivitiesListItem(onNavigateToRecorder: () -> Unit, modifier: Modifier
 @Composable
 fun EmptyActivitiesListItemPreview() {
     ActivelyTheme {
-        EmptyActivitiesListItem(
-            onNavigateToRecorder = { },
-            modifier = Modifier.fillMaxSize()
-        )
+        EmptyActivitiesListItem(onNavigateToRecorder = { })
     }
 }
