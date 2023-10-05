d67592b27958076412a845f46c17b01f33c8a21e
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-05 12:14:49 +0200

    Implement AuthTokensDataSource

--- app/src/main/java/com/actively/datasource/AuthTokensDataSource.kt
+++ app/src/main/java/com/actively/datasource/AuthTokensDataSource.kt
@@ -1,5 +1,8 @@
 package com.actively.datasource
 
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
 interface AuthTokensDataSource {
 
     fun getAccessToken(): String?
@@ -11,4 +14,31 @@ interface AuthTokensDataSource {
     fun setRefreshToken(refreshToken: String)
 
     fun clearTokens()
+}
+
+class AuthTokensDataSourceImpl(
+    private val sharedPreferences: SharedPreferences
+) : AuthTokensDataSource {
+
+    override fun getAccessToken() = sharedPreferences.getString(ACCESS_TOKEN_KEY, null)
+
+    override fun setAccessToken(accessToken: String) = sharedPreferences.edit {
+        putString(ACCESS_TOKEN_KEY, accessToken)
+    }
+
+    override fun getRefreshToken() = sharedPreferences.getString(REFRESH_TOKEN_KEY, null)
+
+    override fun setRefreshToken(refreshToken: String) = sharedPreferences.edit {
+        putString(REFRESH_TOKEN_KEY, refreshToken)
+    }
+
+    override fun clearTokens() = sharedPreferences.edit {
+        putString(ACCESS_TOKEN_KEY, null)
+        putString(REFRESH_TOKEN_KEY, null)
+    }
+
+    private companion object {
+        const val ACCESS_TOKEN_KEY = "access_token"
+        const val REFRESH_TOKEN_KEY = "refresh_token"
+    }
 }
