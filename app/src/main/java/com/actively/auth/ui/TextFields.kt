4c30d8fbed908a1c697f2ac6d116fd1e94e0e2a5
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-05 09:40:59 +0200

    Add toggle password visibility icon button to PasswordTextField

--- app/src/main/java/com/actively/auth/ui/TextFields.kt
+++ app/src/main/java/com/actively/auth/ui/TextFields.kt
@@ -3,15 +3,19 @@ package com.actively.auth.ui
 import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import com.actively.R
 
 @Composable
@@ -40,6 +44,8 @@ fun EmailTextField(
 fun PasswordTextField(
     value: String,
     onValueChange: (String) -> Unit,
+    isPasswordVisible: Boolean,
+    onChangePasswordVisibility: () -> Unit,
     modifier: Modifier = Modifier,
     onDone: (KeyboardActionScope.() -> Unit)? = null,
     isError: Boolean = false,
@@ -50,7 +56,24 @@ fun PasswordTextField(
         onValueChange = onValueChange,
         label = { Text(stringResource(R.string.password)) },
         supportingText = { Text(stringResource(R.string.password_textfield_supporting_text)) },
-        visualTransformation = PasswordVisualTransformation(),
+        trailingIcon = {
+            IconButton(onClick = onChangePasswordVisibility) {
+                Icon(
+                    if (isPasswordVisible) {
+                        painterResource(id = R.drawable.visible)
+                    } else {
+                        painterResource(id = R.drawable.invisible)
+                    },
+                    contentDescription = null
+                )
+            }
+
+        },
+        visualTransformation = if (isPasswordVisible) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        },
         singleLine = true,
         isError = isError,
         keyboardOptions = KeyboardOptions(
