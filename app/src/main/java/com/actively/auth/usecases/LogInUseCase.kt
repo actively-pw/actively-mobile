97d49a978b4e88281cdb01e79e1866412cefdb43
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-06 10:30:11 +0200

    Implement usecases for authentication

--- app/src/main/java/com/actively/auth/usecases/LogInUseCase.kt
+++ app/src/main/java/com/actively/auth/usecases/LogInUseCase.kt
@@ -2,9 +2,27 @@ package com.actively.auth.usecases
 
 import com.actively.auth.AuthResult
 import com.actively.auth.Credentials
+import com.actively.repository.AuthRepository
+import io.ktor.client.plugins.ClientRequestException
 
 interface LogInUseCase {
 
     suspend operator fun invoke(credentials: Credentials): AuthResult
+}
+
+class LogInUseCaseImpl(private val authRepository: AuthRepository) : LogInUseCase {
+
+    override suspend fun invoke(credentials: Credentials) = try {
+        val (accessToken, refreshToken) = authRepository.login(credentials)
+        authRepository.setAccessToken(accessToken)
+        authRepository.setRefreshToken(refreshToken)
+        AuthResult.Success
+    } catch (e: ClientRequestException) {
+        e.printStackTrace()
+        AuthResult.InvalidCredentials
+    } catch (e: Exception) {
+        e.printStackTrace()
+        AuthResult.Error
+    }
 }
 
--- app/src/main/java/com/actively/auth/usecases/LogOutUseCase.kt
+++ app/src/main/java/com/actively/auth/usecases/LogOutUseCase.kt
@@ -1,6 +1,13 @@
 package com.actively.auth.usecases
 
+import com.actively.repository.AuthRepository
+
 interface LogOutUseCase {
 
     suspend operator fun invoke()
+}
+
+class LogOutUseCaseImpl(private val authRepository: AuthRepository) : LogOutUseCase {
+
+    override suspend fun invoke() = authRepository.logout()
 }
