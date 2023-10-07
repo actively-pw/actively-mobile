6ed424f5cbb5c6da7804a0da0e448a187fa2e0db
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-07 12:56:44 +0200

    Add TokensDtoTest

---
+++ app/src/test/java/com/actively/http/dtos/TokensDtoTest.kt
@@ -0,0 +1,23 @@
+package com.actively.http.dtos
+
+import com.actively.auth.Tokens
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class TokensDtoTest : FunSpec({
+
+    isolationMode = IsolationMode.InstancePerTest
+
+    test("Maps TokensDto to Tokens") {
+        val dto = TokensDto("access-token", "refresh-token")
+        val tokens = Tokens("access-token", "refresh-token")
+        dto.toTokens() shouldBe tokens
+    }
+
+    test("Maps Tokens to TokensDto") {
+        val tokens = Tokens("access-token", "refresh-token")
+        val dto = TokensDto("access-token", "refresh-token")
+        tokens.toDto() shouldBe dto
+    }
+})
