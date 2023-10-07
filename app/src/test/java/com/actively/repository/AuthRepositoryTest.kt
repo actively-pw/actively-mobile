8287c5768c0c32bd973fad5a72e61e8d493846fd
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-07 13:29:24 +0200

    Fix AuthRepositoryTest

--- app/src/test/java/com/actively/repository/AuthRepositoryTest.kt
+++ app/src/test/java/com/actively/repository/AuthRepositoryTest.kt
@@ -7,9 +7,9 @@ import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 import io.kotest.matchers.shouldBe
 import io.ktor.client.HttpClient
-import io.mockk.every
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
-import io.mockk.verify
 
 class AuthRepositoryTest : FunSpec({
 
@@ -19,41 +19,41 @@ class AuthRepositoryTest : FunSpec({
     val repository = AuthRepositoryImpl(authTokensDataSource, client)
 
     test("isUserLoggedIn returns true if access and refresh tokens are present") {
-        every { authTokensDataSource.getAccessToken() } returns "access-token"
-        every { authTokensDataSource.getRefreshToken() } returns "refresh-token"
+        coEvery { authTokensDataSource.getAccessToken() } returns "access-token"
+        coEvery { authTokensDataSource.getRefreshToken() } returns "refresh-token"
         repository.isUserLoggedIn().shouldBeTrue()
     }
 
     test("isUserLoggedIn returns false if tokens are missing") {
-        every { authTokensDataSource.getAccessToken() } returns null
-        every { authTokensDataSource.getRefreshToken() } returns null
+        coEvery { authTokensDataSource.getAccessToken() } returns null
+        coEvery { authTokensDataSource.getRefreshToken() } returns null
         repository.isUserLoggedIn().shouldBeFalse()
     }
 
     test("getAccessToken calls AuthTokensDataSource") {
-        every { authTokensDataSource.getAccessToken() } returns "access-token"
+        coEvery { authTokensDataSource.getAccessToken() } returns "access-token"
         repository.getAccessToken() shouldBe "access-token"
-        verify(exactly = 1) { authTokensDataSource.getAccessToken() }
+        coVerify(exactly = 1) { authTokensDataSource.getAccessToken() }
     }
 
     test("setAccessToken calls AuthTokensDataSource") {
         repository.setAccessToken("access-token")
-        verify(exactly = 1) { authTokensDataSource.setAccessToken("access-token") }
+        coVerify(exactly = 1) { authTokensDataSource.setAccessToken("access-token") }
     }
 
     test("getRefreshToken calls AuthTokensDataSource") {
-        every { authTokensDataSource.getRefreshToken() } returns "refresh-token"
+        coEvery { authTokensDataSource.getRefreshToken() } returns "refresh-token"
         repository.getRefreshToken() shouldBe "refresh-token"
-        verify(exactly = 1) { authTokensDataSource.getRefreshToken() }
+        coVerify(exactly = 1) { authTokensDataSource.getRefreshToken() }
     }
 
     test("setRefreshToken calls AuthTokensDataSource") {
         repository.setRefreshToken("refresh-token")
-        verify(exactly = 1) { authTokensDataSource.setRefreshToken("refresh-token") }
+        coVerify(exactly = 1) { authTokensDataSource.setRefreshToken("refresh-token") }
     }
 
     test("logout call datasource to clear tokens") {
         repository.logout()
-        verify(exactly = 1) { authTokensDataSource.clearTokens() }
+        coVerify(exactly = 1) { authTokensDataSource.clearTokens() }
     }
 })
