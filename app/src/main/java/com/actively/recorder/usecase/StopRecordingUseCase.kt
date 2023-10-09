ce4d0264da9a13d522194f1002f6412cb0e94de8
Szymon022 <szymon.kurpas00@gmail.com>
2023-10-09 17:39:28 +0200

    Add StopRecordingUseCase

---
+++ app/src/main/java/com/actively/recorder/usecase/StopRecordingUseCase.kt
@@ -0,0 +1,20 @@
+package com.actively.recorder.usecase
+
+import android.content.Context
+import android.content.Intent
+import com.actively.recorder.RecordActivityService
+
+interface StopRecordingUseCase {
+
+    operator fun invoke()
+}
+
+class StopRecordingUseCaseImpl(private val context: Context) : StopRecordingUseCase {
+
+    override fun invoke() {
+        val stopRecordingIntent = Intent(context, RecordActivityService::class.java).apply {
+            action = RecordActivityService.STOP_ACTION
+        }
+        context.startForegroundService(stopRecordingIntent)
+    }
+}
