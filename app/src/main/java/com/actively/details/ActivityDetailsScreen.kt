6636969b3aa667735be0c0eff1116e484d78eda7
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-12 19:39:04 +0100

    Add stub map item

--- app/src/main/java/com/actively/details/ActivityDetailsScreen.kt
+++ app/src/main/java/com/actively/details/ActivityDetailsScreen.kt
@@ -1,9 +1,18 @@
 package com.actively.details
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -28,5 +37,19 @@ fun NavGraphBuilder.activityDetailsScreen(navController: NavController) {
 
 @Composable
 fun ActivityDetailsScreen() {
-    
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        item {
+            MapItem(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(400.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun MapItem(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.background(color = MaterialTheme.colorScheme.primary)) {
+    }
 }
