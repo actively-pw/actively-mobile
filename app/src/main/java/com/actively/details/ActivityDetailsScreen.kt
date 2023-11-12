76081817983814d66796f07b6bcde8e8dd0478ee
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-12 20:10:06 +0100

    Add details section with stub data and preview

--- app/src/main/java/com/actively/details/ActivityDetailsScreen.kt
+++ app/src/main/java/com/actively/details/ActivityDetailsScreen.kt
@@ -1,21 +1,31 @@
 package com.actively.details
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.actively.recorder.ui.LabeledValue
 import com.actively.ui.theme.ActivelyTheme
 import com.actively.util.BaseScaffoldScreen
 
@@ -51,5 +61,76 @@ fun ActivityDetailsScreen() {
 @Composable
 private fun MapItem(modifier: Modifier = Modifier) {
     Box(modifier = modifier.background(color = MaterialTheme.colorScheme.primary)) {
+    }
+}
+
+@Composable
+private fun DetailsItem(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            LabeledValue(modifier = Modifier.weight(1f), label = "Distance (km)", value = "10.03")
+            Divider(
+                Modifier
+                    .width(1.dp)
+                    .fillMaxHeight()
+            )
+            LabeledValue(
+                modifier = Modifier.weight(1f),
+                label = "Max speed (km/h)",
+                value = "30.41"
+            )
+        }
+        Divider(Modifier.padding(vertical = 4.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            LabeledValue(modifier = Modifier.weight(1f), label = "Total time", value = "01:20:59")
+            Divider(
+                Modifier
+                    .width(1.dp)
+                    .fillMaxHeight()
+            )
+            LabeledValue(modifier = Modifier.weight(1f), label = "Sum of ascent (m)", value = "420")
+        }
+        Divider(Modifier.padding(vertical = 4.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(IntrinsicSize.Min),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            LabeledValue(
+                modifier = Modifier.weight(1f),
+                label = "Avg speed (km/h)",
+                value = "20.59"
+            )
+            Divider(
+                Modifier
+                    .width(1.dp)
+                    .fillMaxHeight()
+            )
+            LabeledValue(
+                modifier = Modifier.weight(1f),
+                label = "Sum of descent (m)",
+                value = "210"
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DetailsPreview() {
+    MaterialTheme {
+        DetailsItem(modifier = Modifier.fillMaxWidth())
     }
 }
