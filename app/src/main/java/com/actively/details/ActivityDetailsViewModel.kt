b60b4932736330a284462bab802b87158d4ed771
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-14 11:32:33 +0100

    Add basic view model

--- app/src/main/java/com/actively/details/ActivityDetailsViewModel.kt
+++ app/src/main/java/com/actively/details/ActivityDetailsViewModel.kt
@@ -1,4 +1,28 @@
 package com.actively.details
 
-class ActivityDetailsViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class ActivityDetailsViewModel : ViewModel() {
+
+    private val _state = MutableStateFlow<DetailsScreenState>(DetailsScreenState.Loading)
+    val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            delay(300)
+            _state.value = DetailsScreenState.Loaded(
+                imageUrl = "",
+                details = listOf(
+                    DetailsRow("Distance (km)" to "10.03", "Max speed (km/h)" to "30.41"),
+                    DetailsRow("Total time" to "01:20:59", "Sum of Ascent (m)" to "420"),
+                    DetailsRow("Avg speed (km/h)" to "20.59", "Sum of descent (m)" to "210"),
+                )
+            )
+        }
+    }
 }
