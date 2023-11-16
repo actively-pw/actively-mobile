a90cba43417ac6661d2f6d901e77bbdfab20005e
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-16 11:14:03 +0100

    Add RecordedActivityUi

---
+++ app/src/main/java/com/actively/home/ui/RecordedActivityUi.kt
@@ -0,0 +1,12 @@
+package com.actively.home.ui
+
+import com.actively.activity.Activity
+import com.actively.activity.RecordedActivity
+
+data class RecordedActivityUi(
+    val id: RecordedActivity.Id,
+    val title: String,
+    val time: String,
+    val stats: Activity.Stats,
+    val mapUrl: String,
+)
