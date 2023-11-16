a0c6cfd51de5836b56e6d7677ee843bb238d8840
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-16 11:57:29 +0100

    Implement get usecase

--- app/src/main/java/com/actively/home/usecase/GetDetailedRecordedActivityUseCase.kt
+++ app/src/main/java/com/actively/home/usecase/GetDetailedRecordedActivityUseCase.kt
@@ -2,8 +2,21 @@ package com.actively.home.usecase
 
 import com.actively.activity.DetailedRecordedActivity
 import com.actively.activity.RecordedActivity
+import com.actively.datasource.RecordedActivitiesDataSource
 
 interface GetDetailedRecordedActivityUseCase {
 
-    suspend operator fun invoke(id: RecordedActivity.Id): DetailedRecordedActivity
+    suspend operator fun invoke(id: RecordedActivity.Id): Result<DetailedRecordedActivity>
+}
+
+class GetDetailedRecordedActivityUseCaseImpl(
+    private val dataSource: RecordedActivitiesDataSource
+) : GetDetailedRecordedActivityUseCase {
+
+    override suspend fun invoke(id: RecordedActivity.Id) = try {
+        Result.success(dataSource.get(id))
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Result.failure(e)
+    }
 }
