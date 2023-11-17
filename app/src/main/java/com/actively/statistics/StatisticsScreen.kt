b462e1c32fd6bf7655cfaec0e36c988111a77061
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-17 13:42:53 +0100

    Add ScrollableTabRow with various disciplines

--- app/src/main/java/com/actively/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/actively/statistics/StatisticsScreen.kt
@@ -19,9 +19,15 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ScrollableTabRow
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -58,6 +64,17 @@ fun NavGraphBuilder.statisticsScreen(navController: NavController) {
 
 @Composable
 fun StatisticsScreen() {
+    val tabs = remember { listOf("Cycling", "Running", "Swimming", "Nordic walking") }
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    ScrollableTabRow(selectedTabIndex = selectedTabIndex) {
+        tabs.forEachIndexed { index, tab ->
+            Tab(
+                selected = index == selectedTabIndex,
+                onClick = { selectedTabIndex = index },
+                text = { Text(text = tab) }
+            )
+        }
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
