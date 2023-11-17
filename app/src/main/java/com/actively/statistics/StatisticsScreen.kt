47a5ae932179a6f6134acf146789181133fb2ec6
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-17 11:58:17 +0100

    Add AvgWeeklyActivity and helper components

--- app/src/main/java/com/actively/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/actively/statistics/StatisticsScreen.kt
@@ -1,13 +1,29 @@
 package com.actively.statistics
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -31,6 +47,7 @@ fun NavGraphBuilder.statisticsScreen(navController: NavController) {
                     )
                 }
             ) {
+                StatisticsScreen()
             }
         }
     }
@@ -38,4 +55,78 @@ fun NavGraphBuilder.statisticsScreen(navController: NavController) {
 
 @Composable
 fun StatisticsScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        AvgWeeklyStats(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        )
+    }
 }
+
+@Composable
+fun AvgWeeklyStats(modifier: Modifier = Modifier) {
+    StatisticsContainer(modifier = modifier, title = "Avg weekly activity") {
+        StatsRow(label = "Rides", value = "0")
+        StatsRow(label = "Time", value = "0 h")
+        StatsRow(label = "Distance", value = "0 km")
+    }
+}
+
+@Composable
+fun StatisticsContainer(
+    title: String,
+    modifier: Modifier = Modifier,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Card(modifier = modifier) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    shape = MaterialTheme.shapes.medium,
+                    color = MaterialTheme.colorScheme.primary
+                )
+                .padding(horizontal = 10.dp),
+            text = title,
+            style = MaterialTheme.typography.headlineMedium,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        Column(modifier = Modifier.padding(horizontal = 10.dp)) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun StatsRow(label: String, value: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(label, style = MaterialTheme.typography.titleLarge)
+        Text(value, style = MaterialTheme.typography.titleLarge)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StatisticsScreenPreview() {
+    ActivelyTheme {
+        StatisticsScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AvgWeekStatsPreview() {
+    ActivelyTheme {
+        AvgWeeklyStats()
+    }
+}
+
