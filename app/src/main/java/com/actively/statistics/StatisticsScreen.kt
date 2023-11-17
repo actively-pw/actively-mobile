0f8933048040f058ed55fb63ba6790b822150d44
Szymon022 <szymon.kurpas00@gmail.com>
2023-11-17 11:00:50 +0100

    Add top bar

--- app/src/main/java/com/actively/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/actively/statistics/StatisticsScreen.kt
@@ -1,5 +1,12 @@
 package com.actively.statistics
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
@@ -7,12 +14,22 @@ import androidx.navigation.compose.composable
 import com.actively.ui.theme.ActivelyTheme
 import com.actively.util.BaseScaffoldScreen
 
+@OptIn(ExperimentalMaterial3Api::class)
 fun NavGraphBuilder.statisticsScreen(navController: NavController) {
     composable("statistics_screen") {
         ActivelyTheme {
             BaseScaffoldScreen(
                 navController = navController,
-                topBar = {}
+                topBar = {
+                    TopAppBar(
+                        title = { Text("Statistics") },
+                        navigationIcon = {
+                            IconButton(onClick = { navController.popBackStack() }) {
+                                Icon(Icons.Default.ArrowBack, contentDescription = null)
+                            }
+                        }
+                    )
+                }
             ) {
             }
         }
