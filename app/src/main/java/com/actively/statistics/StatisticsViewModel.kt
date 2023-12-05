10c6424b3a1862d698008c7fb46fbfa19c4c36b3
Szymon022 <szymon.kurpas00@gmail.com>
2023-12-05 12:08:38 +0100

    Add factory to vm

--- app/src/main/java/com/actively/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/actively/statistics/StatisticsViewModel.kt
@@ -3,22 +3,31 @@ package com.actively.statistics
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.actively.R
-import kotlinx.coroutines.delay
+import com.actively.statistics.usecase.GetStatisticsUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlin.time.Duration.Companion.seconds
 
-class StatisticsViewModel : ViewModel() {
+class StatisticsViewModel(
+    getStatisticsUseCase: GetStatisticsUseCase,
+    statTabFactory: StatTabFactory
+) : ViewModel() {
 
     private val _state = MutableStateFlow(initState())
     val state = _state.asStateFlow()
 
     init {
         viewModelScope.launch {
-            delay(1.seconds)
-            _state.update { it.copy(isLoading = false) }
+            val response = getStatisticsUseCase()
+            val tabs = response.getOrNull()
+            _state.update {
+                it.copy(
+                    tabs = tabs?.map(statTabFactory::create) ?: emptyList(),
+                    isLoading = false,
+                    isError = tabs == null
+                )
+            }
         }
     }
 
@@ -28,64 +37,11 @@ class StatisticsViewModel : ViewModel() {
 
     private fun initState() = StatisticsState(
         tabs = listOf(
-            StatTab(
-                sport = R.string.cycling,
-                avgWeekly = listOf(
-                    LabeledValue(R.string.rides, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                ),
-                yearToDate = listOf(
-                    LabeledValue(R.string.rides, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.elevation_gain, "0 m"),
-                ),
-                allTime = listOf(
-                    LabeledValue(R.string.rides, "0"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.longest_ride, "0 km"),
-                )
-            ),
-            StatTab(
-                sport = R.string.running,
-                avgWeekly = listOf(
-                    LabeledValue(R.string.runs, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                ),
-                yearToDate = listOf(
-                    LabeledValue(R.string.runs, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.elevation_gain, "0 m"),
-                ),
-                allTime = listOf(
-                    LabeledValue(R.string.runs, "0"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.longest_run, "0 km"),
-                )
-            ),
-            StatTab(
-                sport = R.string.nordic_waling,
-                avgWeekly = listOf(
-                    LabeledValue(R.string.walks, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                ),
-                yearToDate = listOf(
-                    LabeledValue(R.string.walks, "0"),
-                    LabeledValue(R.string.time, "0h"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.elevation_gain, "0 m"),
-                ),
-                allTime = listOf(
-                    LabeledValue(R.string.walks, "0"),
-                    LabeledValue(R.string.distance, "0 km"),
-                    LabeledValue(R.string.longest_walk, "0 km"),
-                )
-            )
+            StatTab(sport = R.string.cycling),
+            StatTab(sport = R.string.running),
+            StatTab(sport = R.string.nordic_walking)
         ),
-        isLoading = true
+        isLoading = true,
+        isError = false
     )
 }
