bb61d5866bc69b1efbc568b629b996d7fc47bd64
Szymon022 <szymon.kurpas00@gmail.com>
2023-12-09 12:42:04 +0100

    Add GetStatisticsUseCaseTest

---
+++ app/src/test/java/com/actively/statistics/usecase/GetStatisticsUseCaseTest.kt
@@ -0,0 +1,36 @@
+package com.actively.statistics.usecase
+
+import com.actively.repository.StatisticsRepository
+import com.actively.stubs.stubStatPage
+import io.kotest.core.spec.IsolationMode
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+
+class GetStatisticsUseCaseTest : FunSpec({
+
+    isolationMode = IsolationMode.InstancePerTest
+    val repository = mockk<StatisticsRepository>(relaxed = true)
+    val useCase = GetStatisticsUseCaseImpl(repository)
+
+    test("Calls repository") {
+        coEvery { repository.getAllStatistics() } returns emptyList()
+        useCase()
+        coVerify(exactly = 1) { repository.getAllStatistics() }
+    }
+
+    test("Wraps repository's return in Result.success if no exception was thrown") {
+        val allStats = listOf(stubStatPage(sport = "cycling"), stubStatPage(sport = "running"))
+        coEvery { repository.getAllStatistics() } returns allStats
+        useCase() shouldBe Result.success(allStats)
+    }
+
+    test("Returns Result.failure in case repository throws exception") {
+        val exception = IllegalStateException()
+        coEvery { repository.getAllStatistics() } throws exception
+        useCase() shouldBe Result.failure(exception)
+    }
+})
+
