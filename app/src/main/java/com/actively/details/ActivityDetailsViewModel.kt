7e5d9aaa0dd882f03ae99fcffb97e15a4cf7fd54
Szymon022 <szymon.kurpas00@gmail.com>
2023-12-10 19:15:38 +0100

    Add delete dialogue methods to VM

--- app/src/main/java/com/actively/details/ActivityDetailsViewModel.kt
+++ app/src/main/java/com/actively/details/ActivityDetailsViewModel.kt
@@ -3,6 +3,7 @@ package com.actively.details
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.actively.activity.RecordedActivity
+import com.actively.details.usecase.DeleteActivityUseCase
 import com.actively.distance.Distance.Companion.inKilometers
 import com.actively.distance.Distance.Companion.inWholeMeters
 import com.actively.home.usecase.GetDetailedRecordedActivityUseCase
@@ -11,12 +12,14 @@ import com.actively.util.TimeProvider
 import com.actively.util.getActivityTimeString
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 class ActivityDetailsViewModel(
-    id: RecordedActivity.Id,
+    private val id: RecordedActivity.Id,
     getDetailedRecordedActivityUseCase: GetDetailedRecordedActivityUseCase,
     timeProvider: TimeProvider,
+    private val deleteActivityUseCase: DeleteActivityUseCase
 ) : ViewModel() {
 
     private val _state = MutableStateFlow<DetailsScreenState>(DetailsScreenState.Loading)
@@ -50,6 +53,19 @@ class ActivityDetailsViewModel(
                 )
             } ?: DetailsScreenState.Error
         }
+    }
+
+    fun onDeleteClick() = _state.update {
+        (it as DetailsScreenState.Loaded).copy(showConfirmDeleteDialog = true)
+    }
+
+    fun onConfirmDelete() = viewModelScope.launch {
+        deleteActivityUseCase(id)
+        onDiscardDialogue()
+    }
+
+    fun onDiscardDialogue() = _state.update {
+        (it as DetailsScreenState.Loaded).copy(showConfirmDeleteDialog = false)
     }
 
     private fun Double.trim() = String.format("%.2f", this)
