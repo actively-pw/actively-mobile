52fc4e55c49d1a7d0c16dfc3285c1cbe7b4aa987
Szymon022 <szymon.kurpas00@gmail.com>
2023-12-10 19:09:19 +0100

    Add DeleteActivityUseCase

---
+++ app/src/main/java/com/actively/details/usecase/DeleteActivityUseCase.kt
@@ -0,0 +1,22 @@
+package com.actively.details.usecase
+
+import com.actively.activity.RecordedActivity
+import com.actively.repository.RecordedActivitiesRepository
+
+interface DeleteActivityUseCase {
+
+    suspend operator fun invoke(id: RecordedActivity.Id): Result<Unit>
+}
+
+class DeleteActivityUseCaseImpl(
+    private val repository: RecordedActivitiesRepository
+) : DeleteActivityUseCase {
+
+    override suspend fun invoke(id: RecordedActivity.Id) = try {
+        repository.deleteActivity(id)
+        Result.success(Unit)
+    } catch (e: Exception) {
+        e.printStackTrace()
+        Result.failure(e)
+    }
+}
