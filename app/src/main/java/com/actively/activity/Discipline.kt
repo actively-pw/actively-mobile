2f4579db43b9dc6d059b6d27ee050709f55d2bc6
Szymon022 <szymon.kurpas00@gmail.com>
2023-12-20 10:43:38 +0100

    Add asString and toDiscipline conversion methods for Discipline

--- app/src/main/java/com/actively/activity/Discipline.kt
+++ app/src/main/java/com/actively/activity/Discipline.kt
@@ -6,3 +6,16 @@ sealed class Discipline {
     object Running : Discipline()
     object NordicWalking : Discipline()
 }
+
+fun Discipline.asString() = when (this) {
+    is Discipline.Cycling -> "cycling"
+    is Discipline.Running -> "running"
+    is Discipline.NordicWalking -> "nordic_walking"
+}
+
+fun String.toDiscipline() = when (this.lowercase().trim()) {
+    "cycling" -> Discipline.Cycling
+    "running" -> Discipline.Running
+    "nordic_walking" -> Discipline.NordicWalking
+    else -> error("Unknown discipline: $this.")
+}
